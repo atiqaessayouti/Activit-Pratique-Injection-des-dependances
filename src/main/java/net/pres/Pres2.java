53ab6d5b12270418d38a4969bc09603815139c4e
Essayouti AtiQa <atiqaessayouti@gmail.com>
2026-02-19 01:37:33 +0100

    Partie 1: Implémentation de l'injection dynamique via le setter avec Reflection API

--- src/main/java/net/pres/Pres2.java
+++ src/main/java/net/pres/Pres2.java
@@ -2,23 +2,30 @@ package net.pres;
 
 import net.dao.IDao;
 import net.metier.IMetier;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
+import java.lang.reflect.Method; // Import l-mouhim hna
 
 public class Pres2 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(new File("config"));
-        String daoClassName =scanner.nextLine();;;
+
+        // 1. Instanciation dynamique du DAO
+        String daoClassName = scanner.nextLine();
         Class cDao = Class.forName(daoClassName);
-        IDao d= (IDao) cDao.newInstance();
+        IDao d = (IDao) cDao.getConstructor().newInstance();
 
-        String metierClassName =scanner.nextLine();;;
+        // 2. Instanciation dynamique du Métier
+        String metierClassName = scanner.nextLine();
         Class cMetier = Class.forName(metierClassName);
-        IMetier metier=(IMetier) cMetier.getConstructor(IDao.class).newInstance(d);
-        System.out.println("res"+metier.calcul());
+        IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
 
+
+        //Method setDao = cMetier.getMethod("setDao", IDao.class);
+
+        //setDao.invoke(metier, d);
+
+
+        System.out.println("res=" + metier.calcul());
     }
 }
